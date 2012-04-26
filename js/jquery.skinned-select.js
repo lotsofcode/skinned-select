bf0de1e252ee4b1cfe48e329a18dec84ad83583d
Del <del@lotsofcode.com>
2012-04-26 23:46:32 +0100

    Removed wrapper div in markup, dynamically created by JS

--- js/jquery.skinned-select.js
+++ js/jquery.skinned-select.js
@@ -1,6 +1,10 @@
 ;(function($, undefined) {
     $.SelectSkin = function(element, options) {
-        var defaults = {}
+        var defaults = {
+          textClass : 'skinned-text',
+          selectClass : 'skinned-select',
+          wrapperClass : 'skinned-wrapper'
+        }
         var plugin = this;
         plugin.settings = {}
         var $element = $(element), element = element;
@@ -9,15 +13,17 @@
             convertSelect() 
         }
         var convertSelect = function() {
-            selectContainer = $(element);
+            selectContainer = $(element).wrapAll('<div class="'+plugin.settings.wrapperClass+'" />');
+
             selectContainer.removeClass(this.selector);
-            selectContainer.addClass('skinned-select');
-            selectContainer.children().before('<div class="select-text">Text</div>').each(function() {
-                $(this).prev().text(this.options[0].innerHTML)
+            selectContainer.addClass(plugin.settings.selectClass);
+
+            selectContainer.before('<div class="'+plugin.settings.textClass+'">Text</div>').each(function() {
+                $(this).prev().text(this.options[this.options.selectedIndex].innerHTML)
               }
             );
-            var parentTextObj = selectContainer.children().prev();
-            selectContainer.children().click(function() {
+            var parentTextObj = selectContainer.prev();
+            selectContainer.click(function() {
               parentTextObj.text(this.options[this.selectedIndex].innerHTML);
             })
         }
